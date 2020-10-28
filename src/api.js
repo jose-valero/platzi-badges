139222d8ddfb70449515f72ac478214de0cffc20
Jose Valero <josemiguelvaleroreyes@gmail.com>
2020-10-28 20:46:40 -0300

    fix url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://localhost:9080';
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 const randomNumber = (min = 0, max = 1) =>
